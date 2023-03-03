37f44a9d1e9907861fa3c0924a8f5b5c5f06f540
Hugo Smahlij <smahlij@gmail.com>
2023-03-03 18:22:19 -0300

    added more style

--- main.js
+++ main.js
@@ -45,7 +45,22 @@ clipboardEl.addEventListener("click", () => {
   textarea.select();
   document.execCommand("copy");
   textarea.remove();
-  alert("Password copied to clipboard!");
+  const Toast = Swal.mixin({
+    toast: true,
+    position: "top-end",
+    showConfirmButton: false,
+    timer: 3000,
+    timerProgressBar: true,
+    didOpen: (toast) => {
+      toast.addEventListener("mouseenter", Swal.stopTimer);
+      toast.addEventListener("mouseleave", Swal.resumeTimer);
+    },
+  });
+
+  Toast.fire({
+    icon: "info",
+    title: "Copied in clipboard!",
+  });
 });
 
 // Generate password function
